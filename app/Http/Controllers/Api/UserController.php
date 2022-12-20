424aa42ba6fbd874b86f8a20fdb3b51b288bb996
Thinhlvmor <datlt@mor.com.vn>
2022-12-20 17:14:11 +0700

    Problem php artisan

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return 'All users';
+    }
+    
+    public function getUserDetail(User $user)
+    {
+        return 'User ' . $user;
+    }
+
+    public function createUser(Request $request, User $user)
+    {
+        dd($user->find(1));
+        $rule = [
+            'name' => 'required|min:5',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ];
+    
+        $message = [
+            'name.required' => "Ten bat buoc phai nhap",
+            'name.min' => "Ten phai lon hon :min ki tu",
+            'email.required' => "Email bat buoc phai nhap",
+            'email.email' => "Email khong dung dinh dang",
+            'email.unique' => "Email da ton tai",
+            'password.required' => "Ten bat buoc phai nhap",
+            'password.min' => "Ten phai lon hon :min ki tu",
+        ];
+
+        $request->validate($rule, $message);
+        dd(1);
+    }
+
+    public function updateUser(Request $request)
+    {
+        return $request->all();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('users')->name('users.')->group(function(){
+    Route::get('/', [UserController::class, 'index'])->name('index');
+    Route::get('/{user}', [UserController::class, 'getUserDetail'])->name('detail');
+    Route::post('/create', [UserController::class, 'createUser'])->name('create');
+    Route::put('/{user}', [UserController::class, 'updateUser'])->name('update');
+    Route::delete('/{user}', [UserController::class, 'deleteUser'])->name('delete');
+});
+
+Route::prefix('products')->name('products.')->group(function(){
+    Route::get('/', [ProductsController::class, 'index'])->name('index');
+    Route::get('/{id}', [ProductsController::class, 'detail'])->name('show');
+    Route::post('/create', [ProductsController::class, 'post'])->name('post');
+    Route::put('/{id}', [ProductsController::class, 'update'])->name('update');
+    Route::patch('/{id}', [ProductsController::class, 'patch'])->name('patch');
+    Route::delete('/{id}', [ProductsController::class, 'delete'])->name('delete');
 });
