7ef3edbcc45d5094a51cf7a7c28f60c2883184fd
datquynhvinh <datquynhvinh1231@gmail.com>
2023-04-10 17:43:35 +0700

    Format code

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,6 @@
 config.module.rules.unshift({
     test: /\.m?js$/,
     resolve: {
-      fullySpecified: false, // disable the behavior
+        fullySpecified: false, // disable the behavior
     },
-  });
-MattCrouch, hardrese7, mpsijm, IamFonky, d3vnull10, bongdungyeuem27, zpz1982, emin-rcrypto, serhiiyanhol, and kedarMvyas02 reacted with thumb
+});
